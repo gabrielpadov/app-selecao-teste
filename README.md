b05a6aacd3d7c99ef99f70cb93fa3e4a02c97522
Padovani <gabrielpadov@gmail.com>
2019-05-07 14:39:03 -0300

    build web 7

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 teste
 # teste-selecao
 
-Para upload no servidor web, mudar no package.json ("start": "node server.js")
+Diretório '/api-teste' possui a API.
+    Comandos no diretório
+        npm install
+        npm run dev
+
+Diretório 'teste-selecao'
+    Comandos no diretório
+        npm install
+        ionic serve -l
+Para upload no servidor web, mudar o package.json
+Para compilar local package##.json
+
+Importar dados para o mongoDb
+    Comandos
+        mongoimport --db base --collection endereco --type json --file "/data/enderecos.json"
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*/Install express server
+//Install express server
 const express = require('express');
 const path = require('path');
 
@@ -9,27 +9,8 @@ app.use(express.static(__dirname + '/dist/teste-selecao/'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/teste-selecao/index.html'));
+res.sendFile(path.join(__dirname+'/dist/app-selecao-teste/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-*/
-
-// Install restify server
-const restify = require('restify');
-const path = require('path');
-
-const app = restify();
-
-app.use(restify.static(__dirname + '/dist/teste-selecao/'));
-
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/teste-selecao/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
-
-
