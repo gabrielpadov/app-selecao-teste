5ae8a230cc0b6746a1789eb40a8ba88e73794267
Padovani <gabrielpadov@gmail.com>
2019-05-08 19:32:59 -0300

    deploy to heroku

--- api_teste/config.js
+++ api_teste/config.js
@@ -1,10 +1,10 @@
 
 module.exports = {
     name: 'API',
-    env: process.env.NODE_ENV || 'development',
+    env: process.env.NODE_ENV || 'production',
     port: process.env.PORT || 3000,
     base_url: process.env.BASE_URL || 'http://localhost:3000',
     db: {
-        uri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/api',
+        uri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/api'
     }, 
 };
