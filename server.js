498b9016ebc6f192025f577362c139e6800bf6c0
Padovani <gabrielpadov@gmail.com>
2019-05-09 15:35:33 -0300

    deploy to heroku

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+var morgan = require('morgan');            
+var bodyParser = require('body-parser');    
+var cors = require('cors');
 
 const app = express();
-
+app.use(morgan('dev'));                                        
+app.use(bodyParser.urlencoded({'extended':'true'}));            
+app.use(bodyParser.json());                                     
+app.use(cors());
+ 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/www/'));
 
